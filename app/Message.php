881a30a10eeda5cb8f4a89a9b3a77b98b0fb68ea
Alejandro Sulbaran <sulbaran.kun@gmail.com>
2017-08-27 23:39:13 -0400

    Implementacion de follows y unfollows

--- app/Message.php
+++ app/Message.php
@@ -4,8 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 	class Message extends Model
 	{
 	// messages
 		protected $guarded = [];
+		public  function user()
+        {
+            return $this->belongsTo(User::class);
+        }
 	}
--- app/User.php
+++ app/User.php
@@ -5,6 +5,33 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property string $username
+ * @property string|null $avatar
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\User[] $followers
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\User[] $follows
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Message[] $messages
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUsername($value)
+ */
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -15,7 +42,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name','username','avatar',  'email', 'password',
+        'name', 'username', 'avatar', 'email', 'password',
     ];
 
     /**
@@ -26,4 +53,24 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class)->orderBy('created_at', 'desc');
+    }
+
+    public function follows()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'followed_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'followed_id', 'user_id');
+    }
+
+    public function isFollowing(User $user)
+    {
+        return $this->follows->contains($user);
+    }
 }
