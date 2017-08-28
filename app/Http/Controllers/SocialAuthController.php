63086440c07b38fdb50e665bf3eb691ec41f0093
Alejandro Jose Sulbaran Barrera <sulbaran.kun@gmail.com>
2017-08-28 14:09:45 -0400

    Ultimos toques, y mejoras en el login con facebook

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\SocialProfile;
+use App\User;
 use Illuminate\Http\Request;
 use Socialite;
 
@@ -14,6 +16,37 @@ class SocialAuthController extends Controller
     public function callback()
     {
     	$user = Socialite::driver('facebook')->user();
-    	dd($user);
+    	$existing = User::whereHas('socialProfiles', function ($query) use ($user) {
+    		$query->where('social_id', $user->id);
+	    })->first();
+
+    	if ($existing !== null)
+    	{
+    		auth()->login($existing);
+    		return redirect('/');
+	    }
+    	session()->flash('facebookUser', $user);
+    	return view('users.facebook',[
+		    'user' => $user,
+	    ]);
+    }
+    public function register(Request $request)
+    {
+    	$data = session('facebookUser');
+    	$username= $request->input('username');
+
+    	$user= User::create([
+    		'name'  =>  $data->name,
+		    'email' =>  $data->email,
+		    'avatar' => $data->avatar,
+		    'username'  => $username,
+		    'password' => str_random(16),
+	    ]);
+    	$profile = SocialProfile::create([
+    		'social_id' =>  $data->id,
+		    'user_id'   => $user->id,
+	    ]);
+    	auth()->login($user);
+    	return redirect('/');
     }
 }
--- app/User.php
+++ app/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     {
         return $this->follows->contains($user);
     }
+
+    public function socialProfiles()
+    {
+    	return $this->hasMany(SocialProfile::class);
+    }
 }
