a3d8d3d5f91b5cbb3aed35a80ba6d4d0e058aaa4
Alejandro Jose Sulbaran Barrera <sulbaran.kun@gmail.com>
2017-09-06 15:31:34 -0400

    Incorporacion de Laravel Debug Bar

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         Laravel\Tinker\TinkerServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 	    Laravel\Socialite\SocialiteServiceProvider::class,
+	    Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -228,7 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-	     'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
