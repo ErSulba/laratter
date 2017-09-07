17426fab3f182184314c41d3c09d098086ad94a5
Alejandro Jose Sulbaran Barrera <sulbaran.kun@gmail.com>
2017-09-07 16:10:26 -0400

    Creacion de JSON API para los mensajes y seeders

--- app/Message.php
+++ app/Message.php
@@ -23,29 +23,35 @@ use Laravel\Scout\Searchable;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Message whereUserId($value)
  */
 
-	class Message extends Model
+class Message extends Model
+{
+	use Searchable;
+
+	protected $guarded = [];
+
+	public function user()
 	{
-	// messages
-		use Searchable;
-		protected $guarded = [];
-		public  function user()
-        {
-            return $this->belongsTo(User::class);
-        }
-
-        public function getImageAttribute($image)
-        {
-	        if (!$image || starts_with($image, 'http')) {
-		        return $image;
-	        }
-
-	        return \Storage::disk('public')->url($image);
-        }
-
-        public function toSearchableArray()
-        {
-        	$this->load('user');
-
-        	return $this->toArray();
-        }
+		return $this->belongsTo(User::class);
 	}
+
+	public function getImageAttribute($image)
+	{
+		if (!$image || starts_with($image, 'http')) {
+			return $image;
+		}
+
+		return \Storage::disk('public')->url($image);
+	}
+
+	public function toSearchableArray()
+	{
+		$this->load('user');
+
+		return $this->toArray();
+	}
+
+	public function responses()
+	{
+		return $this->hasMany(Response::class)->latest();
+	}
+}
